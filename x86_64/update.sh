0072505a96d8316f3815cdc79a02edaa9809d59f
Ardox <110931544+LeVraiArdox@users.noreply.github.com>
2023-03-05 23:30:10 +0100

    Create update.sh

---
+++ x86_64/update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+  
+ rm axos* 
+  
+ echo "repo-add" 
+ repo-add -n -R axos.db.tar.gz *.pkg.tar.zst 
+ sleep 5 
+  
+  
+ echo "####################################" 
+ echo "Repo Updated!!" 
+ echo "####################################"
